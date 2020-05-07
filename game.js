acadc82cbfd1e22db0549858212a6e25e2dc81a3
DINH VU Hien <dinhvuhien@MacBook-Pro-de-DINH.local>
2020-05-07 22:45:25 +0200

    new way by Angela

---
+++ game.js
@@ -0,0 +1,56 @@
+let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+
+nextSequence();
+
+
+//Detect click of button
+$(".box").on("click", function () {
+
+  let userChosenColour = $(this).attr("id");
+  //Add to user Pattern
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+
+  animatePress(userChosenColour);
+
+});
+
+
+
+
+
+////////////////////////////
+//FUNCTIONS
+///////////////////////////
+
+
+function nextSequence() {
+  //generate randomNumber between 0 and 3
+  let randomNumber = Math.floor(Math.random() * 4);
+  //Chose the color of the random number
+  let randomChosenColour = buttonColours[randomNumber];
+  //Add to game pattern
+  gamePattern.push(randomChosenColour);
+
+  playSound(randomChosenColour);
+
+  animatePress(randomChosenColour);
+
+}
+
+
+function playSound(name) {
+  //Animate the random button
+  $("#" + name).fadeIn(100).fadeOut(100).fadeIn(100);
+  //Play the random audio
+  (new Audio("sounds/" + name + ".mp3")).play();
+}
+
+
+function animatePress(currentColour){
+  $("#"+currentColour).addClass("pressed");
+  setTimeout(()=>{ $("#"+currentColour).removeClass("pressed"); }, 100);
+}
